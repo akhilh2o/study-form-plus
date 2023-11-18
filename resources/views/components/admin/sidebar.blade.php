642ae5e8b5fdd71d755dea9e58f777c17dafdae8
akhilh2o <d.akhil.gupta@com>
2023-11-18 12:04:08 +0530

    changes

--- resources/views/components/admin/sidebar.blade.php
+++ resources/views/components/admin/sidebar.blade.php
@@ -25,6 +25,7 @@
                                     <a href="{{ route('admin.categories.create') }}">
                                         New Categories
                                     </a>
+                                    
                                 </li>
                             @endcan
                             @can('category_create')
